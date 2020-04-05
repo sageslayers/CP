29b7f385d4b1b4f3cbf3e32c9d8df388760584f1
sageslayers <37839473+sageslayers@users.noreply.github.com>
2020-04-05 14:13:10 +0700

    Update 1196B.cpp

--- Codeforces/gitgud/1196B.cpp
+++ Codeforces/gitgud/1196B.cpp
@@ -3,7 +3,9 @@
 using namespace std ;
 
 int main () { 
-
+ios_base::sync_with_stdio(false) ; 
+cin.tie(NULL) ; 
+cout.tie(NULL) ; 
 int t ; 
 cin >> t;  
 while ( t-- ) { 
@@ -16,8 +18,28 @@ while ( t-- ) {
 			if( vect[i] % 2 == 1 )
 				odd++ ; 
 		}
-	
-	
+		//cout << odd << endl ; 
+	if( odd < k || (odd%2 != k%2) ) {
+		cout << "No" << endl ; 
+		continue ; 
+	}
+	else {
+		cout << "Yes" << endl ; 
+		int count = 0 ; 
+		for( int i = 0 ; i < n ; i ++ ) {
+			if( count == k-1 ){
+				cout << n << endl;
+				break ;
+			}
+			if( vect[i] % 2 == 1 ) {
+				cout << i+1 << " "   ;
+				count ++ ;  } 
+			
+			
+		}
+			
+		
+	}
 }	
 	
 }
